55ae9dbfa0f3a95b74099348cc418dceed42dbd0
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-02 12:49:10 +0530

    Add files via upload

---
+++ ServiceTrackerApp.Tests/TestCases/BoundaryTests.cs
@@ -0,0 +1,272 @@
+﻿
+using ServiceTrackerApp.DAL;
+using ServiceTrackerApp.DAL.Interface;
+using ServiceTrackerApp.DAL.Repository;
+using ServiceTrackerApp.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ServiceTrackerApp.Tests.TestCases
+{
+    public class BoundaryTests
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly IServiceTrackerInterface _serviceTrackerService;
+        public readonly Mock<IServiceTrackerRepository> servicetrackerservice = new Mock<IServiceTrackerRepository>();
+        private readonly Service _Service;
+        private readonly IEnumerable<Service> ServiceList;
+
+        private static string type = "Boundary";
+
+        public BoundaryTests(ITestOutputHelper output)
+        {
+            _serviceTrackerService = new ServiceTrackerService(servicetrackerservice.Object);
+            _output = output;
+            _Service = new Service
+            {
+                 Id = 1,
+                Date = DateTime.Now,
+                ServiceName = "Exercise",
+                IsCompleted = true,
+                Notes = "30 minutes of jogging in the park."
+            };
+            ServiceList = new List<Service>
+        {
+            new Service
+            {
+                Id = 1,
+                Date = DateTime.Now,
+                ServiceName = "Exercise",
+                IsCompleted = true,
+                Notes = "30 minutes of jogging in the park."
+            },
+            new Service
+            {
+                Id = 2,
+                Date = DateTime.Now,
+                ServiceName = "Exercise",
+                IsCompleted = true,
+                Notes = "30 minutes of jogging in the park."
+            },
+        };
+
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_Id_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                servicetrackerservice.Setup(repos => repos.GetServiceByID(_Service.Id)).Returns(_Service);
+                var result = _serviceTrackerService.GetServiceByID(_Service.Id);
+
+                //Assertion
+                if (result!= null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_Date_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                servicetrackerservice.Setup(repos => repos.GetServiceByID(_Service.Id)).Returns(_Service);
+                var result = _serviceTrackerService.GetServiceByID(_Service.Id);
+
+                //Assertion
+                if (result!= null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_ServiceName_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                servicetrackerservice.Setup(repos => repos.GetServiceByID(_Service.Id)).Returns(_Service);
+                var result = _serviceTrackerService.GetServiceByID(_Service.Id);
+
+                //Assertion
+                if (result!=null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_IsCompleted_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                servicetrackerservice.Setup(repos => repos.GetServiceByID(_Service.Id)).Returns(_Service);
+                var result = _serviceTrackerService.GetServiceByID(_Service.Id);
+               
+
+                //Assertion
+                if (result!= null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_Notes_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                servicetrackerservice.Setup(repos => repos.GetServiceByID(_Service.Id)).Returns(_Service);
+                var result = _serviceTrackerService.GetServiceByID(_Service.Id);
+
+
+                //Assertion
+                if (result != null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+    }
+}
---
+++ ServiceTrackerApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ServiceTrackerApp.MvcApplication" Language="C#" %>
